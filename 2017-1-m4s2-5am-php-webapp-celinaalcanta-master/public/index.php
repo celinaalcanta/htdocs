81c018a94ab459e697cb99a2f6b27d2dfd39af4a
celinaalcanta <31414155+celinaalcanta@users.noreply.github.com>
2017-09-29 10:03:36 -0600

    Autoload

--- 2017-1-m4s2-5am-php-webapp-celinaalcanta-master/public/index.php
+++ 2017-1-m4s2-5am-php-webapp-celinaalcanta-master/public/index.php
@@ -1,48 +1,6 @@
 <?php
-require_once '../src/app/db/mysql.php';
-require_once '.../src/models/user.php';
-use models\user;
-if(isset($_POST['usuario'])&& isset($_POST['passwd'])){
-    $usuario =$_POST['usuario'];
-    $passwd =$_POST['passwd'];
-    $usuario =$_POST['passwd'];
-}
-?>
+require '../config.inc.php';
+require '../src/app/autoload.php';
+use Src\Models\User;
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<?php
-   if isset($usuario) && $usuario) {
-       echo "Usuario $usuario ->name puede iniciar seción correctamente";
-   }
-    <div style ="width:300px;">
-    <form action="#" id="loginform" method="post"
->      <fieldset >
-         <legend>Login Form</legend>
-        <!-- div>label[for=usuario]{usuario:}+input[type=text name=usuario id=usuario]
-       -->
-   
-         <label for="usuario">usuario:</label>
-         <imput type="text" name ="usuario" id="usuario">
-         <!-- div> label [for=passwd] {contraseña:}+input[type=password name=passwd id=passwd] -->
-         <div>
-           <label for="passwd"> contraseña:</label>
-           <input type="pasword" name="passwd" id="passwd">
-         </div>
-         <div><a href="remember.php">¿Olvidaste tu contraseña?</a></div>
-         <button type="submit">Iniciar seción></button>
-         <hr>
-         ¿No tienes una cuenta?
-         <a href="signup.pgp">Registrate aquí</a>
-         </fieldset>
-         </div>
-         </div>
-</body>
-</html>
+$usuario = new User();
