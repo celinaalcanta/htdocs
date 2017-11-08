511b0aac94ff06878133e2854f03b8af5e079ffe
celinaalcanta <31414155+celinaalcanta@users.noreply.github.com>
2017-11-08 08:26:34 -0700

    bienvenido a biblioteca pusimos eso

--- 2017-1-m4s2-5am-php-webapp-celinaalcanta-master/public/index.php
+++ 2017-1-m4s2-5am-php-webapp-celinaalcanta-master/public/index.php
@@ -2,11 +2,12 @@
 require_once '../config.inc.php';
 require_once '../src/app/helpers.php';
 require_once '../src/app/autoload.php';
-use src\app\Router;
+//use src\app\Router;
 
-if (isset($_GET['url'])) {
-   $url = $_GET['url'];
-   Router::get($url);
-} else {
-   Router::get('home');
-}
+//if (isset($_GET['url'])) {
+  // $url = $_GET['url'];
+   //Router::get($url);
+//} else {
+ //  Router::get('home');
+//}
+echo 'Bienvenido a biblioteca-CETis108';
